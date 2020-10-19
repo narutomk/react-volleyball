c9ef4485378544743c46cfcb4c53420a896e54b5
narutomk <khaled.bouflija@gmail.com>
2020-10-19 19:56:14 +0100

    Updates

---
+++ precache-manifest.5c01bdffe25e369fcfc136905a4116c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c51251130819824b3820f89ffde758",
+    "url": "/react-volleyball/index.html"
+  },
+  {
+    "revision": "cc408db0efcf37b6255e",
+    "url": "/react-volleyball/static/css/main.62927a30.chunk.css"
+  },
+  {
+    "revision": "4b576a48db40e6fa9bc2",
+    "url": "/react-volleyball/static/js/2.ce9d694f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-volleyball/static/js/2.ce9d694f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc408db0efcf37b6255e",
+    "url": "/react-volleyball/static/js/main.ac26df98.chunk.js"
+  },
+  {
+    "revision": "3aee98e84b520c016572",
+    "url": "/react-volleyball/static/js/runtime-main.a07ba230.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44aea7c12a5b1e3ed1d89078b7497e81.js"
+  "/react-volleyball/precache-manifest.5c01bdffe25e369fcfc136905a4116c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-volleyball/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
